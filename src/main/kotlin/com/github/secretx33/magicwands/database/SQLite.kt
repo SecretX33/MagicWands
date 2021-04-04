8fc6cd53fb19e48abc5dd3f1d182900f9440a3c7
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2021-04-04 15:43:41 -0300

    sql skeleton made

--- src/main/kotlin/com/github/secretx33/magicwands/database/SQLite.kt
+++ src/main/kotlin/com/github/secretx33/magicwands/database/SQLite.kt
@@ -24,15 +24,13 @@ class SQLite(plugin: Plugin) {
     private fun initialize() {
         try {
             ds.connection.use { conn: Connection ->
-                conn.prepareStatement(CREATE_QUEST_CHESTS).use { it.execute() }
-                conn.prepareStatement(CREATE_CHEST_CONTENT).use { it.execute() }
-                conn.prepareStatement(CREATE_PLAYER_PROGRESS).use { it.execute() }
-                conn.prepareStatement(CREATE_TRIGGER).use { it.execute() }
+                conn.prepareStatement(CREATE_SPELLTEACHER).use { it.execute() }
+                conn.prepareStatement(CREATE_LEARNED_SPELLS).use { it.execute() }
                 conn.commit()
                 log.fine("Initiated DB")
             }
         } catch (e: SQLException) {
-            log.fine(("${ChatColor.RED}ERROR: An exception occurred while trying to connect to the database and create the tables\n${e.stackTraceToString()}"))
+            log.severe(("${ChatColor.RED}ERROR: An exception occurred while trying to connect to the database and create the tables\n${e.stackTraceToString()}"))
         }
     }
 
@@ -47,26 +45,22 @@ class SQLite(plugin: Plugin) {
         val hikariConfig = HikariConfig().apply { isAutoCommit = false }
 
         // create tables
-        const val CREATE_QUEST_CHESTS = "CREATE TABLE IF NOT EXISTS questChests(location VARCHAR(150) NOT NULL PRIMARY KEY, chest_order INTEGER NOT NULL);"
-        const val CREATE_CHEST_CONTENT = "CREATE TABLE IF NOT EXISTS chestContents(id INTEGER PRIMARY KEY, chest_location INTEGER NOT NULL, player_uuid VARCHAR(60) NOT NULL, inventory VARCHAR(500000) NOT NULL, FOREIGN KEY(chest_location) REFERENCES questChests(location));"
-        const val CREATE_PLAYER_PROGRESS = "CREATE TABLE IF NOT EXISTS playerProgress(player_uuid VARCHAR(60) NOT NULL PRIMARY KEY, progress INTEGER NOT NULL);"
-        const val CREATE_TRIGGER = "CREATE TRIGGER IF NOT EXISTS removeInventories BEFORE DELETE ON questChests FOR EACH ROW BEGIN DELETE FROM chestContents WHERE chestContents.chest_location = OLD.location; END"
+        const val CREATE_SPELLTEACHER = "CREATE TABLE IF NOT EXISTS spellTeacher(location VARCHAR(150) NOT NULL PRIMARY KEY, spell_type VARCHAR(80) NOT NULL, block_material VARCHAR(80) NOT NULL);"
+        const val CREATE_LEARNED_SPELLS = "CREATE TABLE IF NOT EXISTS learnedSpells(player_uuid VARCHAR(60) NOT NULL PRIMARY KEY, known_spells VARCHAR(800) NOT NULL);"
         // selects
-        const val SELECT_ALL_FROM_QUEST_CHEST = "SELECT * FROM questChests;"
-        const val SELECT_ALL_FROM_PLAYER_PROGRESS = "SELECT * FROM playerProgress;"
-        const val SELECT_CHEST_CONTENT = "SELECT inventory FROM chestContents WHERE chest_location = ? AND player_uuid = ? LIMIT 1;"
-        const val SELECT_PLAYER_PROCESS = "SELECT progress FROM playerProgress WHERE player_uuid = ? LIMIT 1;"
+        const val SELECT_ALL_FROM_SPELLTEACHER = "SELECT * FROM spellTeacher;"
+        const val SELECT_LEARNED_SPELLS= "SELECT known_spells FROM learnedSpells WHERE player_uuid = ? LIMIT 1;"
         // inserts
-        const val INSERT_QUEST_CHEST = "INSERT INTO questChests(location, chest_order) VALUES (?, ?);"
-        const val INSERT_CHEST_CONTENTS = "INSERT INTO chestContents(chest_location, player_uuid, inventory) VALUES (?, ?, ?);"
-        const val INSERT_PLAYER_PROGRESS = "INSERT INTO playerProgress(player_uuid, progress) VALUES (?, ?);"
+        const val INSERT_SPELLTEACHER = "INSERT INTO spellTeacher(location, spell_type, block_material) VALUES (?, ?, ?);"
+        const val INSERT_LEARNED_SPELLS = "INSERT INTO learnedSpells(player_uuid, known_spells) VALUES (?, ?);"
         // updates
-        const val UPDATE_QUEST_CHEST_ORDER = "UPDATE questChests SET chest_order = ? WHERE location = ?;"
-        const val UPDATE_CHEST_CONTENTS = "UPDATE chestContents SET inventory = ? WHERE chest_location = ? AND player_uuid = ?;"
-        const val UPDATE_PLAYER_PROGRESS = "UPDATE playerProgress SET progress = ? WHERE player_uuid = ?;"
+        const val UPDATE_SPELLTEACHER = "UPDATE spellTeacher SET spell_type = ? WHERE location = ?;"
+        const val UPDATE_LEARNED_SPELLS = "UPDATE learnedSpells SET known_spells = ? WHERE player_uuid = ?;"
         // removes
-        const val REMOVE_QUEST_CHESTS_OF_WORLD = "DELETE FROM questChests WHERE location LIKE ?;"
-        const val REMOVE_QUEST_CHEST = "DELETE FROM questChests WHERE location = ?;"
-        const val REMOVE_PLAYER_PROGRESS = "DELETE FROM playerProgress WHERE player_uuid = ?;"
+        const val REMOVE_SPELLTEACHER_OF_WORLD = "DELETE FROM spellTeacher WHERE location LIKE ?;"
+        const val REMOVE_SPELLTEACHER = "DELETE FROM spellTeacher WHERE location = ?;"
+        const val REMOVE_LEARNED_SPELLS = "DELETE FROM learnedSpells WHERE player_uuid = ?;"
+
+        val UUID_WORLD_PATTERN = """^"\{\\"world\\":\\"([0-9a-zA-Z-]+).*""".toPattern()
     }
 }
