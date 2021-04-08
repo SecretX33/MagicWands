SecretX33/MagicWands

commit 6a33f44000417957b8692564a28ec994eb752c89

Added fall damage immunity after leaping
Fixed small issue in SQLite class

build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 }
 
 group = "com.github.secretx33"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     jcenter()
@@ -74,7 +74,7 @@ tasks.register<proguard.gradle.ProGuardTask>("proguard") {
     configuration("proguard-rules.pro")
 }
 
-tasks.build.get().finalizedBy(tasks.getByName("proguard"))
+//tasks.build.get().finalizedBy(tasks.getByName("proguard"))
 
 tasks.withType<JavaCompile> { options.encoding = "UTF-8" }
 

src/main/kotlin/com/github/secretx33/magicwands/eventlisteners/spellcasts/SpellCastListener.kt
@@ -4,6 +4,7 @@ import com.github.secretx33.magicwands.config.Config
 import com.github.secretx33.magicwands.config.ConfigKeys
 import com.github.secretx33.magicwands.config.MessageKeys
 import com.github.secretx33.magicwands.config.Messages
+import com.github.secretx33.magicwands.eventlisteners.sideeffectsmitigation.FallDamageListener
 import com.github.secretx33.magicwands.events.SpellCastEvent
 import com.github.secretx33.magicwands.manager.SpellFuelManager
 import com.github.secretx33.magicwands.manager.SpellManager
@@ -29,6 +30,7 @@ class SpellCastListener (
     private val fuelManager: SpellFuelManager,
     private val spellManager: SpellManager,
     private val learnedSpells: LearnedSpellsRepo,
+    private val fallDamageListener: FallDamageListener,
     private val config: Config,
     private val messages: Messages,
 ) : Listener {
@@ -69,12 +71,13 @@ class SpellCastListener (
         if(isFuelEnabled) fuelManager.consumeFuel(player, spellType)
 
         when(spellType){
-            LEAP -> spellManager.castLeap(this)
+            LEAP -> spellManager.castLeap(this).also { fallDamageListener.addFallDamageImmunity(player) }
             VANISH -> spellManager.castVanish(this)
             else -> {}
         }
         ItemUtils.increaseCastCount(wand)
-        if(isCooldownsEnabled) spellManager.addSpellCD(player, spellType)
+        val cd = spellManager.getSpellCD(player, spellType)
+        if(isCooldownsEnabled && cd > 0) spellManager.addSpellCD(player, spellType)
     }
 
     private fun Player.canSendCDMessage(): Boolean {

src/main/kotlin/com/github/secretx33/magicwands/manager/SpellManager.kt
@@ -11,6 +11,7 @@ import com.github.secretx33.magicwands.model.Cuboid
 import com.github.secretx33.magicwands.model.SpellType
 import com.github.secretx33.magicwands.model.TempModification
 import com.github.secretx33.magicwands.utils.runSync
+import com.google.common.cache.CacheBuilder
 import kotlinx.coroutines.*
 import org.bukkit.Location
 import org.bukkit.Material
@@ -27,6 +28,7 @@ import org.koin.core.component.KoinApiExtension
 import java.lang.StrictMath.pow
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.TimeUnit
 import kotlin.collections.HashSet
 import kotlin.math.*
 
@@ -40,15 +42,16 @@ class SpellManager (
     private val wgChecker: WorldGuardChecker,
 ) {
 
-    private val cooldown = HashMap<Pair<UUID, SpellType>, Long>()
+    private val cooldown = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES).build<Pair<UUID, SpellType>, Long>()
+//    private val cooldown = HashMap<Pair<UUID, SpellType>, Long>()
     private val tempModification = ConcurrentHashMap<Job, TempModification>()
     private val blocksBlackList = HashSet<Location>()
 
     fun getSpellCD(player: Player, spellType: SpellType): Long
-        = max(0L, (cooldown.getOrDefault(Pair(player.uniqueId, spellType), 0L) - System.currentTimeMillis()))
+        = max(0L, (cooldown.getIfPresent(Pair(player.uniqueId, spellType)) ?: 0L) - System.currentTimeMillis())
 
     fun addSpellCD(player: Player, spellType: SpellType) {
-        cooldown[Pair(player.uniqueId, spellType)] = System.currentTimeMillis() + config.get(spellType.configCooldown, 7.0).toLong() * 1000
+        cooldown.put(Pair(player.uniqueId, spellType), System.currentTimeMillis() + (config.get(spellType.configCooldown, 7.0) * 1000).toLong())
     }
 
     fun castBlind(event: EntitySpellCastEvent) {

src/main/resources/config.yml
@@ -33,6 +33,8 @@ remove-spellteacher-from-db-if-world-not-found: true
 cooldown-for-notifying-spell-is-cd: 2.0
 # If true and ProtocolLib is present, players will only be able to see their own dropped wands on the ground. This config will also ignore /wand reload because of limitations.
 players-only-see-their-own-wands: true
+# Prevent fall damage after using leap for X seconds, increase if you feel like it's not enough. This config will ignore /wand reload.
+player-immune-to-fall-damage-after-leaping-for: 3.5
 
 ############################################################
 # +------------------------------------------------------+ #

src/main/kotlin/com/github/secretx33/magicwands/MagicWands.kt
@@ -6,9 +6,10 @@ import com.github.secretx33.magicwands.config.ConfigKeys
 import com.github.secretx33.magicwands.config.Messages
 import com.github.secretx33.magicwands.database.SQLite
 import com.github.secretx33.magicwands.eventlisteners.*
+import com.github.secretx33.magicwands.eventlisteners.sideeffectsmitigation.FallDamageListener
+import com.github.secretx33.magicwands.eventlisteners.sideeffectsmitigation.FireworkDamageWorkaroundListener
 import com.github.secretx33.magicwands.eventlisteners.spellcasts.BlockSpellCastListener
 import com.github.secretx33.magicwands.eventlisteners.spellcasts.EntitySpellCastListener
-import com.github.secretx33.magicwands.eventlisteners.spellcasts.FireworkDamageWorkaroundListener
 import com.github.secretx33.magicwands.eventlisteners.spellcasts.SpellCastListener
 import com.github.secretx33.magicwands.eventlisteners.spellteacher.SpellTeacherBreakListener
 import com.github.secretx33.magicwands.eventlisteners.spellteacher.SpellTeacherUseListener
@@ -49,18 +50,19 @@ class MagicWands : JavaPlugin(), CustomKoinComponent {
         single { HiddenPlayersHelper(get()) }
         single { ParticlesHelper(get(), get(), get(named("firework"))) }
         single { SpellManager(get(), get(), get(), get(), get(), get()) }
-        single { SpellTeacherBreakListener(get(), get(), get()) }
+        single { FallDamageListener(get(), get()) }
+        single { FireworkDamageWorkaroundListener(get(), get(named("firework"))) }
         single { BlockSpellCastListener(get(), get(), get()) }
         single { EntitySpellCastListener(get(), get()) }
-        single { FireworkDamageWorkaroundListener(get(), get(named("firework"))) }
-        single { PlayerDeathListener(get(), get(), get()) }
-        single { PlayerLeaveListener(get(), get()) }
-        single { WandPreventCraftListener(get(), get()) }
-        single { WandPickupPreventListener(get()) }
-        single { SpellCastListener(get(), get(), get(), get(), get(), get()) }
+        single { SpellCastListener(get(), get(), get(), get(), get(), get(), get()) }
+        single { SpellTeacherBreakListener(get(), get(), get()) }
         single { SpellTeacherUseListener(get(), get(), get(), get(), get(), get()) }
+        single { WandPickupPreventListener(get()) }
+        single { WandPreventCraftListener(get(), get()) }
         single { WandSpellSwitchListener(get(), get(), get()) }
         single { WandUseListener(get(), get(), get(), get(), get()) }
+        single { PlayerDeathListener(get(), get(), get()) }
+        single { PlayerLeaveListener(get(), get()) }
         single { Commands(get()) }
         single { WandDropPacketListener(get()) }
     }
@@ -68,31 +70,31 @@ class MagicWands : JavaPlugin(), CustomKoinComponent {
     override fun onLoad() {
         // if worldguard is enabled, replace dummy module with real one
         if(isWorldGuardEnabled) {
-            // forces the creation of the WorldGuardChecker here because WG is bae and requires hooks to
+            // creation of the WorldGuardChecker happens here because WG is bae and requires hooking to happen on method onLoad
             val wgChecker = WorldGuardCheckerImpl(logger)
             mod.single<WorldGuardChecker>(override = true) { wgChecker }
         }
     }
 
     override fun onEnable() {
-        val economy = server.servicesManager.load(Economy::class.java)?: throw IllegalStateException("Vault was not found")
+        val economy = server.servicesManager.load(Economy::class.java) ?: throw IllegalStateException("Vault was not found")
         mod.single(override = true) { economy }
         startKoin {
             printLogger(Level.ERROR)
             loadKoinModules(mod)
         }
-        get<SpellTeacherBreakListener>()
+        get<FallDamageListener>()
+        get<FireworkDamageWorkaroundListener>()
         get<BlockSpellCastListener>()
         get<EntitySpellCastListener>()
-        get<FireworkDamageWorkaroundListener>()
         get<SpellCastListener>()
+        get<SpellTeacherBreakListener>()
+        get<SpellTeacherUseListener>()
+        get<WandPickupPreventListener>()
+        get<WandPreventCraftListener>()
+        get<WandSpellSwitchListener>()
         get<PlayerDeathListener>()
         get<PlayerLeaveListener>()
-        get<WandPreventCraftListener>()
-        get<WandPickupPreventListener>()
-        get<SpellCastListener>()
-        get<SpellTeacherUseListener>()
-        get<WandSpellSwitchListener>()
         get<WandUseListener>()
         get<Commands>()
         if(wandsAreExclusive) get<WandDropPacketListener>()

src/main/kotlin/com/github/secretx33/magicwands/config/Config.kt
@@ -42,6 +42,7 @@ enum class ConfigKeys(val configEntry: String, val defaultValue: Any) {
     DELETE_WAND_ON_DEATH("delete-wand-on-death", false),
     DISABLE_ALL_COOLDOWNS("disable-all-cooldowns", false),
     DISABLE_FUEL_USAGE("disable-fuel-usage", false),
+    FALL_DAMAGE_IMMUNE_PERIOD("player-immune-to-fall-damage-after-leaping-for", 3.0),
     ENABLE_EFFECTS("enable-spell-effects", true),
     ENABLE_MESSAGE_ON_SPELL_SWITCH("enable-message-on-spell-switch", true),
     PLAYERS_ONLY_SEE_THEIR_OWN_WANDS("players-only-see-their-own-wands", true),

src/main/kotlin/com/github/secretx33/magicwands/database/SQLite.kt
@@ -183,7 +183,6 @@ class SQLite(plugin: Plugin, private val config: Config) {
         try {
             withQueryStatement(SELECT_LEARNED_SPELLS, {
                 setString(1, playerUuid.toString())
-                executeQuery()
             }) { rs ->
                 if(rs.next()) return rs.getString("known_spells").toSpellTypeSet()
             }

src/main/kotlin/com/github/secretx33/magicwands/eventlisteners/sideeffectsmitigation/FallDamageListener.kt
@@ -1,0 +1,41 @@
+package com.github.secretx33.magicwands.eventlisteners.sideeffectsmitigation
+
+import com.github.secretx33.magicwands.config.Config
+import com.github.secretx33.magicwands.config.ConfigKeys
+import com.github.secretx33.magicwands.utils.isPlayer
+import com.google.common.cache.CacheBuilder
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.plugin.Plugin
+import java.util.concurrent.TimeUnit
+import kotlin.math.max
+
+class FallDamageListener (plugin: Plugin, config: Config) : Listener {
+
+    private val immunePeriod: Long = (max(0.0, config.get<Double>(ConfigKeys.FALL_DAMAGE_IMMUNE_PERIOD) * 1000)).toLong()
+
+    init { Bukkit.getPluginManager().registerEvents(this, plugin) }
+
+    private val immuneList = CacheBuilder.newBuilder()
+        .expireAfterWrite(immunePeriod, TimeUnit.MILLISECONDS)
+        .build<Player, Long>()
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    private fun EntityDamageEvent.onPlayerFallDamage() {
+        if(!isPlayerFallDamage() || immunePeriod == 0L) return
+
+        val player = entity as Player
+        if(player.isImmuneToFallDamage())
+            isCancelled = true
+    }
+
+    fun addFallDamageImmunity(player: Player) = immuneList.put(player, System.currentTimeMillis() + immunePeriod)
+
+    private fun Player.isImmuneToFallDamage(): Boolean = immuneList.getIfPresent(this).let { it != null && (it + immunePeriod) > System.currentTimeMillis() }.also { if(it) immuneList.invalidate(this) }
+
+    private fun EntityDamageEvent.isPlayerFallDamage() = cause == EntityDamageEvent.DamageCause.FALL && entity.isPlayer()
+}

commit d51ee5b37259dad9c742e11e2429467ca4f809ce

Fixed issue with CDs not working
CD bug was introduced in commit https://github.com/SecretX33/MagicWands/commit/6a33f44000417957b8692564a28ec994eb752c89 because of a misuse of function SpellManager#getSpellCD, which got renamed to 'getPlayerCDSpell' to avoid future confusion.

Added dynamic cooldown (guava cache list) list that gets replaced with a new one that reflects the new CDs each time '/wand reload' is used. Bumped version to 1.0.4.

build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 }
 
 group = "com.github.secretx33"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     jcenter()

src/main/kotlin/com/github/secretx33/magicwands/commands/subcommands/ReloadCommand.kt
@@ -3,6 +3,7 @@ package com.github.secretx33.magicwands.commands.subcommands
 import com.github.secretx33.magicwands.config.Config
 import com.github.secretx33.magicwands.config.MessageKeys
 import com.github.secretx33.magicwands.config.Messages
+import com.github.secretx33.magicwands.manager.SpellManager
 import com.github.secretx33.magicwands.utils.CustomKoinComponent
 import com.github.secretx33.magicwands.utils.inject
 import org.bukkit.command.CommandSender
@@ -18,6 +19,7 @@ class ReloadCommand : SubCommand(), CustomKoinComponent {
 
     private val config by inject<Config>()
     private val messages by inject<Messages>()
+    private val spellManager by inject<SpellManager>()
 
     override fun onCommandByPlayer(player: Player, alias: String, strings: Array<String>) {
         onCommandByConsole(player, alias, strings)
@@ -26,6 +28,7 @@ class ReloadCommand : SubCommand(), CustomKoinComponent {
     override fun onCommandByConsole(sender: CommandSender, alias: String, strings: Array<String>) {
         config.reload()
         messages.reload()
+        spellManager.reload()
         sender.sendMessage(messages.get(MessageKeys.CONFIGS_RELOADED))
     }
 

src/main/kotlin/com/github/secretx33/magicwands/config/Messages.kt
@@ -56,7 +56,7 @@ enum class MessageKeys(val default: String) {
     FORGOT_SPELL("${ChatColor.GREEN}You forgot spell <spell>!"),
     GOT_POISONED("${ChatColor.GREEN}You have been poisoned by <caster>."),
     GOT_SLOWED("${ChatColor.GREEN}You have been slowed by <caster>."),
-    HAVENT_BOUGHT_THIS_MATERIAL_SKIN("${ChatColor.RED}You may not turn <item> into a wand, you haven't bought it's skin yet."),
+    HAVENT_BOUGHT_THIS_MATERIAL_SKIN("${ChatColor.RED}You may not turn <item> into a wand, you haven't bought its skin yet."),
     INVALID_SKIN_NAME("${ChatColor.RED}There is no skin named <skin>, please try again."),
     INVALID_WAND_MATERIAL("${ChatColor.RED}<item> cannot be an wand, please use a <allowed_material>."),
     ITEM_NOT_A_WAND("${ChatColor.RED}This item is not a wand!"),

src/main/kotlin/com/github/secretx33/magicwands/eventlisteners/spellcasts/SpellCastListener.kt
@@ -55,7 +55,7 @@ class SpellCastListener (
         }
 
         // still in cooldown
-        val cd = spellManager.getSpellCD(player, spellType)
+        val cd = spellManager.getPlayerCDSpell(player, spellType)
         if(cd > 0) {
             isCancelled = true
             if(!player.canSendCDMessage()) return
@@ -76,8 +76,8 @@ class SpellCastListener (
             else -> {}
         }
         ItemUtils.increaseCastCount(wand)
-        val cd = spellManager.getSpellCD(player, spellType)
-        if(isCooldownsEnabled && cd > 0) spellManager.addSpellCD(player, spellType)
+        val cd = config.get(spellType.configCooldown, 0.0)
+        if(isCooldownsEnabled && cd > 0) spellManager.addPlayerCDSpell(player, spellType)
     }
 
     private fun Player.canSendCDMessage(): Boolean {

src/main/kotlin/com/github/secretx33/magicwands/manager/SpellManager.kt
@@ -42,16 +42,22 @@ class SpellManager (
     private val wgChecker: WorldGuardChecker,
 ) {
 
-    private val cooldown = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES).build<Pair<UUID, SpellType>, Long>()
-//    private val cooldown = HashMap<Pair<UUID, SpellType>, Long>()
+    private var spellCooldowns = buildCooldownList()
     private val tempModification = ConcurrentHashMap<Job, TempModification>()
     private val blocksBlackList = HashSet<Location>()
 
-    fun getSpellCD(player: Player, spellType: SpellType): Long
-        = max(0L, (cooldown.getIfPresent(Pair(player.uniqueId, spellType)) ?: 0L) - System.currentTimeMillis())
+    fun reload() { spellCooldowns = buildCooldownList().apply { putAll(spellCooldowns.asMap()) } }
 
-    fun addSpellCD(player: Player, spellType: SpellType) {
-        cooldown.put(Pair(player.uniqueId, spellType), System.currentTimeMillis() + (config.get(spellType.configCooldown, 7.0) * 1000).toLong())
+    private val highestSpellCDInMs
+        get() = SpellType.values().map { config.get(it.configCooldown, 0.0) }.maxOrNull()?.plus(1)?.times(1000)?.toLong() ?: 0
+
+    private fun buildCooldownList() = CacheBuilder.newBuilder().expireAfterWrite(highestSpellCDInMs, TimeUnit.MILLISECONDS).build<Pair<UUID, SpellType>, Long>()
+
+    fun getPlayerCDSpell(player: Player, spellType: SpellType): Long
+        = max(0L, (spellCooldowns.getIfPresent(Pair(player.uniqueId, spellType)) ?: 0L) - System.currentTimeMillis())
+
+    fun addPlayerCDSpell(player: Player, spellType: SpellType) {
+        spellCooldowns.put(Pair(player.uniqueId, spellType), System.currentTimeMillis() + (config.get(spellType.configCooldown, 7.0) * 1000).toLong())
     }
 
     fun castBlind(event: EntitySpellCastEvent) {

src/main/resources/config.yml
@@ -33,7 +33,7 @@ remove-spellteacher-from-db-if-world-not-found: true
 cooldown-for-notifying-spell-is-cd: 2.0
 # If true and ProtocolLib is present, players will only be able to see their own dropped wands on the ground. This config will also ignore /wand reload because of limitations.
 players-only-see-their-own-wands: true
-# Prevent fall damage after using leap for X seconds, increase if you feel like it's not enough. This config will ignore /wand reload.
+# Prevent fall damage after using leap for X seconds, increase if you feel like it's not enough. It only provides immunity to fall damage once, it's like a token, once you used it, it's gone and no longer works. This config will ignore /wand reload.
 player-immune-to-fall-damage-after-leaping-for: 3.5
 
 ############################################################

src/main/resources/messages.yml
@@ -40,7 +40,7 @@ console-cannot-use: "&cSorry, the console cannot use this command."
 forgot-spell: "&aYou forgot spell <spell>!"
 got-poisoned: "&aYou have been poisoned by <caster>."
 got-slowed: "&aYou have been slowed by <caster>."
-havent-bought-this-material-skin: "&cYou may not turn <item> into a wand, you haven't bought it's skin yet."
+havent-bought-this-material-skin: "&cYou may not turn <item> into a wand, you haven't bought its skin yet."
 invalid-skin-name: "&cThere is no skin named <skin>, please try again."
 invalid-wand-material: "&c<item> cannot be an wand, please use a <allowed_material>."
 item-not-a-wand: "&cThis item is not a wand!"